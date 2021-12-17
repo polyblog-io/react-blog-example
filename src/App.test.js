e53b9c4e3042b2f419fd513f9435cb56e5ec9b26
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2021-12-17 13:35:35 +0100

    fix: fix test by searching for Blog

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const blogTitle = screen.getByText(/blog/i)
+  expect(blogTitle).toBeInTheDocument()
 })
